8e2cdd87ff02de9d0b68a725a55e28898f54eae7
Buwei Chen <buwei.chen@yale.edu>
2024-09-12 15:34:49 -0400

    change pwd to script location

--- raspberrypi/test.sh
+++ raspberrypi/test.sh
@@ -3,6 +3,7 @@
 # wanIp="$(curl https://ipinfo.io/ip 2>/dev/null)";
 
 # echo "Your public ip is: ${wanIp}";
+cd "$(dirname "$0")";
 echo "Your public ip is: $(hostname -I)";
 hostname -I > ip.md;
 git add ip.md;
